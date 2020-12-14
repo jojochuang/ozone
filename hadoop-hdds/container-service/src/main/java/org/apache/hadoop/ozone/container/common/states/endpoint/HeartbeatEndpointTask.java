cabc955ce34e67fe5b56cbf779487256e2956bb0
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2020-12-14 03:28:22 -0800

    HDDS-4404. Datanode can go OOM when a Recon or SCM Server is very slow in processing reports (#1601)
    * HDDS-4404. Datanode can go OOM when a Recon or SCM Server is very slow in processing reports.
    
    Change-Id: Idf6394d0bde2fb1c9fe99a96c49fa28677e0e527
    
    * Checkstyle.
    
    Change-Id: I6c4729634905bf6bba5bb6483c393d3c7511a78e
    
    * Add mock-maker-inline so UT can mock final classes.
    
    Change-Id: I63470510ed2089438f08f4d3cea321c1b5f01c3c
    
    * Remove throw exception in addReport.
    
    Change-Id: Id3ff6222ed279568d32876afc0a6a5ee1316c926
    
    * Fix existing UT TestStateContext#testReportAPIs.
    
    Change-Id: I25020b4411f4163a2ec134431793bfe663e5f6bf
    
    * Remove unused imports.
    
    Change-Id: I2dc93c42fc7468b37d41a179ca047ee385fc8896
    
    * Work around mockito bug in UT TestCreatePipelineCommandHandler: caused by adding mock-maker-inline.
    
    Change-Id: I0f21c508af1f7b36d7632aaa3b9173a17056f211
    
    * Add UT testContainerNodePipelineReportAPIs.
    
    Change-Id: I8d67a1b6e2d9043fb1479e2547d2e690932f7985
    
    * Clean up.
    
    Change-Id: Iaec544ba6fb211d0d71fe03b26b0fc70199dc882
    
    * @VisibleForTesting for get container/node/pipeline reports as they are only used in UT, at least at the moment.
    
    Change-Id: I72070fde8df220ecebf2fb6d1015d19f65cee11e
    
    * - Renamed `reports` to `incrementalReportsQueue` to better reflect the functionality of the map.
    - Added `acceptedIncrementalReportTypeSet` for rigorous report type checking.
    - `StateContext#addReport` now explicitly throws, instead of hiding any possible NPEs. Rationale: As I think again, those reports are all internally generated rather than dependent on direct user input. It's better to catch any problem early (if any).
    - `StateContext#addReport` also strictly checks for report type now. This intends to force any future additions of report types to be explicitly allowed. Only 5 report types (names) are expected:
    ```
    hadoop.hdds.NodeReportProto
    hadoop.hdds.ContainerReportsProto
    hadoop.hdds.IncrementalContainerReportProto
    hadoop.hdds.CommandStatusReportsProto
    hadoop.hdds.PipelineReportsProto
    ```
    - `StateContext#putBackReports` now checks ALL reports in the list to be put back. I'd expect the list to be small. And since the logic is run on each DataNode, it shouldn't become a performance bottleneck. Also added debug logging just in case we need to diagnose it in the future.
    - Added new UT `testPutBackReports` and `testReportQueueWithAddReports` to check that `putBackReports` and `getReports` behaves.
    
    Change-Id: I48aca3d4a422d52532b39c9462a30519c9cbe0eb
    
    * Make containerReports, nodeReport, pipelineReports atomic and final.
    
    Change-Id: I44aba0ff093fa0ff5f9a35a74ff4126d9c73dd7b
    
    * Checkstyle.
    
    Change-Id: I545ca8c636b8bbd1239a936603b6efcbfbd10b88
    
    * Clean up.
    
    Change-Id: I5cdc38a297cf3d91bad8335eb36f2ba6b2009c10
    
    * Remove assertion in putBackReports as requestBuilder might include Container/Node/Pipeline reports.
    
    Thanks Aravindan.
    
    Change-Id: Ic39f53de8bb91a179bd5dc3301aa89ffecf5c076
    
    * Empty commit to retrigger CI.
    
    Change-Id: I46fb65ce2c81cb607dd10a9ee829d829ec7a8cfc
    
    * Empty commit to retrigger CI.
    
    Change-Id: I9d3b5ce0b81d457344ecd6b4ad6e141169bd89d3
    
    * Retrigger.
    
    Change-Id: I4e9dace08152e21c2bb5da564487d776a84c4ade
    
    * Allow null as input to StateContext#addReport() as this blocked UT TestStorageContainerManager#testBlockDeletionTransactions.
    
    Change-Id: I16c8bfcfbd619a23241ea6dda9b95265714984bb
    
    * Retrigger CI
    
    Change-Id: I64203ebbaeebdb4c598bb8dad578579c82bf604b
    
    * trigger new CI check
    
    Co-authored-by: Doroszlai, Attila <adoroszlai@apache.org>
    (cherry picked from commit 0152eb5a728aaccf23cefccdd4f406b5fd45b6ca)

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/states/endpoint/HeartbeatEndpointTask.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/states/endpoint/HeartbeatEndpointTask.java
@@ -141,15 +141,15 @@ public class HeartbeatEndpointTask
       if (LOG.isDebugEnabled()) {
         LOG.debug("Sending heartbeat message :: {}", request.toString());
       }
-      SCMHeartbeatResponseProto reponse = rpcEndpoint.getEndPoint()
+      SCMHeartbeatResponseProto response = rpcEndpoint.getEndPoint()
           .sendHeartbeat(request);
-      processResponse(reponse, datanodeDetailsProto);
+      processResponse(response, datanodeDetailsProto);
       rpcEndpoint.setLastSuccessfulHeartbeat(ZonedDateTime.now());
       rpcEndpoint.zeroMissedCount();
     } catch (IOException ex) {
+      Preconditions.checkState(requestBuilder != null);
       // put back the reports which failed to be sent
       putBackReports(requestBuilder);
-
       rpcEndpoint.logIfNeeded(ex);
     } finally {
       rpcEndpoint.unlock();
@@ -160,12 +160,9 @@ public class HeartbeatEndpointTask
   // TODO: Make it generic.
   private void putBackReports(SCMHeartbeatRequestProto.Builder requestBuilder) {
     List<GeneratedMessage> reports = new LinkedList<>();
-    if (requestBuilder.hasContainerReport()) {
-      reports.add(requestBuilder.getContainerReport());
-    }
-    if (requestBuilder.hasNodeReport()) {
-      reports.add(requestBuilder.getNodeReport());
-    }
+    // We only put back CommandStatusReports and IncrementalContainerReport
+    // because those are incremental. Container/Node/PipelineReport are
+    // accumulative so we can keep only the latest of each.
     if (requestBuilder.getCommandStatusReportsCount() != 0) {
       reports.addAll(requestBuilder.getCommandStatusReportsList());
     }
@@ -193,6 +190,7 @@ public class HeartbeatEndpointTask
           } else {
             requestBuilder.setField(descriptor, report);
           }
+          break;
         }
       }
     }
