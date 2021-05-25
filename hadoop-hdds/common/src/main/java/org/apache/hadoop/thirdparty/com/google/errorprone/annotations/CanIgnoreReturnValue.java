4aa9c4dce9b98802eb5b45a4f3e46af4d67a8d6f
Wei-Chiu Chuang <weichiu@cloudera.com>
2021-05-25 22:48:39 +0800

    Add missing class in hadoop-thirdparty.
    Change-Id: Iabc54f1bacff8a20a07748fc29477f28a8222ff6

---
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/thirdparty/com/google/errorprone/annotations/CanIgnoreReturnValue.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package org.apache.hadoop.thirdparty.com.google.errorprone.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+public @interface CanIgnoreReturnValue {
+}
