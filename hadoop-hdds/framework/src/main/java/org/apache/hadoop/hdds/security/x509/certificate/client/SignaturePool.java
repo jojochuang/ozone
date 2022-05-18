08759ee0ad6f0f0e7374a8460f89b009d6d66d7b
Wei-Chiu Chuang <weichiu@apache.org>
2022-05-18 14:54:31 -0700

    Fix javadoc, add apche license and ensure objects are deallocated when the pool is GC'ed.
    Change-Id: I343ad1e5b8b855327b44bc1bf4bf96137799c31a

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/client/SignaturePool.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/client/SignaturePool.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.hadoop.hdds.security.x509.certificate.client;
 
 import org.apache.commons.pool2.PooledObject;
@@ -8,6 +27,16 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import java.security.Signature;
 
+/**
+ * Creates a pool of Signature classes.
+ * Objects are pre-allocated at initialization and are not evicted if idle.
+ *
+ * Due to the cost of object allocation at initialization, this pool is expected
+ * to be long-lived.
+ *
+ * It is based on Apache Commons Pool. For more details, check out the
+ * <a href="https://commons.apache.org/proper/commons-pool/">website</a>.
+ */
 public class SignaturePool extends GenericObjectPool<Signature> {
 
   public static class SignatureFactory
@@ -68,5 +97,14 @@ public class SignaturePool extends GenericObjectPool<Signature> {
     SignatureFactory factory = new SignatureFactory(algorithm, provider);
 
     return new SignaturePool(factory, config);
+  }
+
+  /**
+   * When the pool is garbage collected, close the resources and destroy
+   * the objects in the pool.
+   */
+  @Override
+  public void finalize() {
+    this.close();
   }
 }
